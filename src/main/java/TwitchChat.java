0554fd52a1d69c73ed2570b52dc11db517346fe1
limzj05 <limzijing05@gmail.com>
2026-08-15 22:01:02 +0800

    Change chatbot name to TwitchChat

---
+++ src/main/java/TwitchChat.java
@@ -0,0 +1,8 @@
+public class TwitchChat {
+    public static void main(String[] args) {
+        String banner = "+-----------------------------+\n"
+                + "|         TwitchChat          |\n"
+                + "+-----------------------------+\n";
+        System.out.println(banner);
+    }
+}
