ff1da2b747f1cd91c5f14a218eaa5a1177fc7621
limzj05 <limzijing05@gmail.com>
2026-08-23 19:39:05 +0800

    Add TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class TaskList {
+    private Task[] tasks;
+    private int taskCount;
+
+    public TaskList() {
+        this.tasks = new Task[100];
+        this.taskCount = 0;
+    }
+
+    public void addTask(String taskName) {
+        tasks[taskCount] = new Task(taskName);
+        taskCount++;
+    }
+
+    public Task getTasks(int taskId) {
+        return tasks[taskId - 1]; // Convert 1-indexed taskId to 0-index for accessing task
+    }
+
+
+    public int getTaskCount() {
+        return taskCount;
+    }
+
+    public void printTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.printf("%d.[%s] %s\n", i + 1, tasks[i].getStatusIcon(), tasks[i].getTaskName());
+        }
+    }
 }
