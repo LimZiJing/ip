61dd3ed7350b714aa15d91be8d594c9147c95b15
limzj05 <limzijing05@gmail.com>
2026-08-23 17:14:00 +0800

    Add task storage and task display

--- src/main/java/TwitchChat.java
+++ src/main/java/TwitchChat.java
@@ -13,18 +13,38 @@ public class TwitchChat {
         System.out.println("How can I help you?");
         System.out.println(horizontalLine);
 
-        // Get user input
         Scanner inputScanner = new Scanner(System.in);
+        String[] tasks = new String[100]; // Array to store tasks
+        int taskCount = 0;
+        // Get user input
         while (true) {
             String userInput = inputScanner.nextLine();
             if (userInput.equalsIgnoreCase("bye")) {
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+
+                System.out.println(horizontalLine);
+
+                // Print out tasks stored
+                if (taskCount == 0) {
+                    System.out.println("No tasks currently.");
+                } else {
+                    System.out.println("Here are your tasks: ");
+                    for (int i = 0; i < taskCount; i++){
+                        System.out.printf("%d: %s\n", i + 1, tasks[i]);
+                    }
+                }
+
+                System.out.println(horizontalLine);
+            } else {
+                // Store user input as task
+                tasks[taskCount] = userInput;
+                taskCount++;
+                System.out.println(horizontalLine);
+                System.out.printf("added: %s\n", userInput);
+                System.out.println(horizontalLine);
             }
 
-            // Echo user input
-            System.out.println(horizontalLine);
-            System.out.println(userInput);
-            System.out.println(horizontalLine);
         }
 
 
