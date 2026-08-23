491487afe9603ab295dc35b9148c39e313ee71c5
limzj05 <limzijing05@gmail.com>
2026-08-23 19:37:10 +0800

    Make Task fields private and add getters and setters

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,19 @@
 public class Task {
-    protected String taskName;
-    protected boolean isDone;
+
+    private String taskName;
+    private boolean isDone;
 
     public Task(String taskName) {
         this.taskName = taskName;
         this.isDone = false;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
     }
 
     public String getStatusIcon() {
