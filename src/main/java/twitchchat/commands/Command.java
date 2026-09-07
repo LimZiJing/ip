4947ee6a82e11b830394cd4c20cbf06feb0f556e
LimZiJing <e1528395@u.nus.edu>
2026-09-08 00:07:38 +0800

    Command: add taskId getter

--- src/main/java/twitchchat/commands/Command.java
+++ src/main/java/twitchchat/commands/Command.java
@@ -4,24 +4,29 @@ public class Command {
     private final CommandType type;
     private final String[] arguments;
     private final int id;
+
     public Command(CommandType type, String[] arguments) {
         this.type = type;
         this.arguments = arguments;
         this.id = 0;
     }
 
-    public Command(CommandType type, int id){
+    public Command(CommandType type, int id) {
         this.type = type;
         this.arguments = new String[0];
         this.id = id;
     }
 
-    public CommandType getType(){
+    public CommandType getType() {
         return this.type;
     }
 
     public String[] getArguments() {
         return arguments.clone();
+    }
+
+    public int getId() {
+        return id;
     }
 
 }
